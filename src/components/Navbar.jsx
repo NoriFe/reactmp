141939b7c2f36b1d0288f47ee7a80b66ffc7bcc1
NoriFe <norbert.fe.dev@gmail.com>
2023-08-23 20:57:43 +0100

    intial commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,17 @@
+import { Stack } from '@mui/material';
+import { Link } from 'react-router-dom';
+
+import logo from '../utils/constants';
+
+const Navbar = () => (
+ <Stack 
+ direction="row"
+  alignItem="center" 
+  p={2} 
+  sx={{ position: 'sticky', background: '#000', top: 0, justifyContent: 'space-between'}}>
+
+ </Stack>
+  )
+
+
+export default Navbar
