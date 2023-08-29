f50191223fa316ed84a9d4b8aaf6e5a64f4adc80
NoriFe <norbert.fe.dev@gmail.com>
2023-08-29 15:39:01 +0100

    adding side bar

---
+++ src/components/Feed.jsx
@@ -0,0 +1,29 @@
+import React, { useEffect, useState } from "react";
+import { Box, Stack, Typography } from "@mui/material";
+
+import Sidebar from "./Sidebar";
+
+
+const Feed = () => {
+  
+
+  return (
+    <Stack sx={{ flexDirection: 
+    { sx: "column", md: "row" } }}>
+      <Box sx={{ height: { sx: "auto", md: "92vh" }, 
+      borderRight: "1px solid red", px: { sx: 0, md: 2 } }}>
+        <Sidebar />
+        <Typography className="copyright" variant="body2" sx={{ mt: 1.5, color: "#fff", }}>
+          Copyright © Nori Media
+        </Typography>
+     
+
+      
+
+        
+      </Box>
+    </Stack>
+  );
+};
+
+export default Feed;
---
+++ src/components/VideoDetail.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const S = () => {
+  return (
+    <div>S</div>
+  )
+}
+
+export default S
