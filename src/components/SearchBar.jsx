8595e206e9d822f86ff85d5036406772a97c9266
NoriFe <norbert.fe.dev@gmail.com>
2023-08-30 10:33:15 +0100

    added loader and fix map issue

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,14 +1,26 @@
 import React, { useState } from 'react';
 import { useNavigate } from "react-router-dom";
 import { Paper, IconButton } from '@mui/material';
-import Search from '@mui/icons-material/Search';
+import SearchIcon from '@mui/icons-material/Search';
 
 const SearchBar = () => {
+  const [searchTerm, setSearchTerm] = useState('');
+  const navigate = useNavigate();
+
+  const onhandleSubmit = (e) => {
+    e.preventDefault();
+
+    if (searchTerm) {
+      navigate(`/search/${searchTerm}`);
+
+      setSearchTerm('');
+    }
+  };
 
   return (
     <Paper
       component='form'
-      onSubmit={() => {}}
+      onSubmit={onhandleSubmit}
       sx={{
         borderRadius: 20,
         border: '1px solid #e3e3e3',
@@ -20,11 +32,11 @@ const SearchBar = () => {
       <input
         className='search-bar'
         placeholder='Search...'
-        value=""
-        onChange={() => {}}
+        value={searchTerm}
+        onChange={(e) => setSearchTerm(e.target.value)}
       />
       <IconButton type='submit' sx={{ p: '10px', color: 'red' }} aria-label='search'>
-        <Search />
+        <SearchIcon />
       </IconButton>
     </Paper>
   );
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,39 +1,36 @@
-import { Stack } from '@mui/material';
-import { categories } from '../utils/constants';
-import { Category } from '@mui/icons-material';
+import React from "react";
+import { Stack } from "@mui/material";
 
-const selectedCategory = 'New';
+import { categories } from "../utils/constants";
 
-const Sidebar = () => (
-<Stack 
-direction= "row"
-sx={{
-  overflowY: "auto",
-  height: { sx: 'auto', md: '95%'},
-  flexDirection: { md: 'column'},
-}} >
-
-
-  {categories.map((Category) => (
-      <button className="category-btn"
-      style={{
-        background: Category.name === selectedCategory && '#FC1503',
-        color: 'white'
-      }}
-      key={Category.name}
+const Categories = ({ selectedCategory, setSelectedCategory }) => (
+  <Stack
+    direction="row"
+    sx={{
+      overflowY: "auto",
+      height: { sx: "auto", md: "95%" },
+      flexDirection: { md: "column" },
+    }}
+  >
+    {categories.map((category) => (
+      <button
+        className="category-btn"
+        onClick={() => setSelectedCategory(category.name)}
+        style={{
+          background: category.name === selectedCategory && "#FC1503",
+          color: "white",
+        }}
+        key={category.name}
       >
-        <span style={{
-          color: Category.name === selectedCategory ? 'white' :
-          'red', marginRight: '15px'}}>
-            {Category.icon}</span>
-        <span style={{
-           opacity: Category.name === selectedCategory ? '1' : '0.8'}}>
-            {Category.name}</span>
+        <span style={{ color: category.name === selectedCategory ? "white" : "red", marginRight: "15px" }}>
+          {category.icon}
+        </span>
+        <span style={{ opacity: category.name === selectedCategory ? "1" : "0.8" }}>
+          {category.name}
+        </span>
       </button>
-  ))}
-</Stack>
-)
+    ))}
+  </Stack>
+);
 
-
-
-export default Sidebar
+export default Categories;
